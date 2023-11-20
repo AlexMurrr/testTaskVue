6eed17ed113f70073448a1f7821f90154866022f
Alex Mur <lehamuraschov2014@yandex.ru>
2023-11-20 14:44:48 +0200

    feat: create array of object - label and value , v-model from inputs to it and create component InputValue.vue

--- src/components/InputValue.vue
+++ src/components/InputValue.vue
@@ -8,15 +8,7 @@
 export default {
   name: "InputValue",
   data() {
-    return {
-      isNone: false,
-      fullHeight: false,
-      items:[{label: 'Фамилия', value: ''},
-             {label:'Имя', value:''},
-             {label:'Отчество', value:''},
-             {label:'Специальность', value:''},
-             {label:'Опыт', value:''},]
-    };
-   }
+
+    }
   }
 </script>
